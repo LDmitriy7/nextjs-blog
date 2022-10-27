5cc2263fb826afb80aa881ebeeb83a321a31784c
levch <levchenko.d.a1998@gmail.com>
2022-10-27 23:42:18 +0600

    v 1.7

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import '/styles/global.css'
-import {AppProps, NextWebVitalsMetric} from "next/app";
+import {AppProps, NextWebVitalsMetric} from 'next/app'
+import {Analytics} from '@vercel/analytics/react'
 
 export default ({Component, pageProps}: AppProps) => (
-  <Component {...pageProps}/>
+  <>
+    <Component {...pageProps}/>
+    <Analytics/>
+  </>
 )
 
 export function reportWebVitals(metric: NextWebVitalsMetric) {
